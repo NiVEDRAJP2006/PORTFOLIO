747cdb634370ff73854eb6983b98d6e518d524d4
NIVU <nivedr445@gmail.com>
2026-03-23 19:57:06 +0530

    Fixed responsive design

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 let menuIcon = document.querySelector('#menu-icon');
-let navabar = document.querySelector('.navbar');
+let mobileNav = document.querySelector('.mobile-nav');
+let overlay = document.querySelector('.menu-overlay');
+let closeBtn = document.querySelector('.nav-close');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navabar.classList.toggle('active');
-}
+    menuIcon.classList.toggle('open');
+    mobileNav.classList.toggle('open');
+    overlay.classList.toggle('show');
+};
+
+overlay.onclick = () => {
+    menuIcon.classList.remove('open');
+    mobileNav.classList.remove('open');
+    overlay.classList.remove('show');
+};
+
+closeBtn.onclick = () => {
+    menuIcon.classList.remove('open');
+    mobileNav.classList.remove('open');
+    overlay.classList.remove('show');
+};
